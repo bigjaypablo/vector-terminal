81b2e7d9a5ce2263a7e73e04f0d67c49a9f4ea4f
Big Jay <johnsoncarter50@gmail.com>
2026-09-05 22:09:26 +0100

    Fix duplicate NetworkBanner import/usage that broke the build

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import NetworkBanner from "./NetworkBanner";
-import NetworkBanner from "./NetworkBanner";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import MobileNavigation from "./MobileNavigation";
@@ -17,8 +16,7 @@ export default function AppShell({ children }) {
   return (
     <div className="h-screen flex flex-col bg-[#0a0a0a] text-white">
       <Topbar />
-        <NetworkBanner />
-        <NetworkBanner />
+      <NetworkBanner />
       <div className="flex flex-1 min-h-0">
         <Sidebar collapsed={collapsed} onToggle={() => setCollapsed((c) => !c)} />
         <main className="flex-1 min-w-0 overflow-y-auto pb-16 md:pb-0">
